f71be6cb1981e5268e91f00c0f9ad6c5e7af7e44
shalbibil <syifashalsabilah@gmail.com>
2026-06-02 21:37:16 +0700

    Update koneksi.php

--- dana-kas-desa/koneksi.php
+++ dana-kas-desa/koneksi.php
@@ -1,1 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "kades_db";
 
+// Membuat koneksi
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+// Cek koneksi
+if (!$koneksi) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+?>
